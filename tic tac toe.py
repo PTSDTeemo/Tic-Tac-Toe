13ea39ce9fc727bcf0259b460cf29aef047c48bd
Teemo <38870829+PTSDTeemo@users.noreply.github.com>
2018-05-01 21:27:42 -0400

    Add files via upload

--- tic tac toe.py
+++ tic tac toe.py
@@ -1,15 +1,16 @@
 gameOver = 0
 who = 'x'   #Player x goes first. Player o goes second.
 takenMoves = []
-grid = list('''7|8|9
------
-4|5|6
------
-1|2|3''')
-#grid indexes that I care about: 0  2  4
-#                               12 14 16
-#                               24 26 28
-# (all the other indexes are whitespace)
+grid = list('7|8|9' + '\n' +
+            '-----' + '\n' +
+            '4|5|6' + '\n' +
+            '-----' + '\n' +
+            '1|2|3')
+            #grid indexes that I care about:
+            #  0  2  4
+            # 12 14 16
+            # 24 26 28
+            # (all the other indexes are whitespace)
 
 
 
@@ -23,8 +24,10 @@ while not gameOver:
     while move in takenMoves:
         print ('\n' + move + ' is taken. Choose again:\n\n')
         for c in grid: print(c, end='')
-        print ('\n')
+        print ()
         move = input()
+
+
 
     grid[(grid.index(move))] = who      #Replaces an index with x or o
     takenMoves.append(move)             #Updates list of taken moves
@@ -43,17 +46,39 @@ while not gameOver:
         (grid[0] == who and grid[14] == who and grid[28] == who) or
         (grid[4] == who and grid[14] == who and grid[24] == who)
         ):
-        print ('You win!\n\n')
+        print ('\n' + who + ' wins!\n\n')
         for c in grid: print(c, end='')
         gameOver = 1
-    elif len(takenMoves) == 9:      #Tie-checker
-        print('Draw. ', end='')     #
-        gameOver = 1                #
+
+    #Tie-checker
+    elif len(takenMoves) == 9:
+        print('\nDraw. ', end='')
+        gameOver = 1
     else:
         print()
         if who == 'x':
             who = 'o'
         else:
             who = 'x'
+
+
+    #See if they want to play again. Reset variables. Loser goes first.
+    if gameOver:
+        bestOfThree = input('Play again? y / n\n')
+        if bestOfThree == 'y':
+            gameOver = 0
+            takenMoves = []
+            grid = list('7|8|9' + '\n' +
+                        '-----' + '\n' +
+                        '4|5|6' + '\n' +
+                        '-----' + '\n' +
+                        '1|2|3')
+            if who == 'x':
+                who = 'o'
+            else:
+                who = 'x'
+        elif bestOfThree != 'n':
+            print('Bad!' * 20)
+            
 
 print('\n\nGame over.')
